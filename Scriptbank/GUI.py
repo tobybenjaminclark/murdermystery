aa25d8cef7c34c35ba14f3c366cbbb0a572fcccb
Toby Clark <psytc9@nottingham.ac.uk>
2022-10-29 13:57:57 +0100

    add gui

--- Scriptbank/GUI.py
+++ Scriptbank/GUI.py
@@ -8,10 +8,29 @@ class GUI():
         self.master.geometry("1920x1080")
         self.master.attributes('-fullscreen','true')
 
-        self.label = Label(self.master, text="Hello Tkinter!")
-        self.label.pack()
+        self.create_generator()
 
         self.master.update()
         self.master.mainloop()
 
+        # Selection Bar
+        # Right Upper Display
+        # Right Lower Display
+        # Left Infoview
+
+    def create_generator(self):
+        self.generator_elements = []
+
+        # Number of Rooms Slider
+        self.room_var = DoubleVar()
+        self.room_scale = Scale(self.master,
+            variable = self.room_var, 
+            from_ = 1,
+            to = 100, 
+            length = 500,
+            sliderlength = 10,
+            orient = HORIZONTAL)  
+
+        self.room_scale.pack()
+        
 p = GUI()
